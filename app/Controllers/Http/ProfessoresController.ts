9ff6f4de1f33790ea9400d0502a04a6b0e6ec5c6
Renan Rego <renannevesr@gmail.com>
2023-03-18 15:07:35 -0300

    CRUD Professores

--- app/Controllers/Http/ProfessoresController.ts
+++ app/Controllers/Http/ProfessoresController.ts
@@ -1,3 +1,59 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Professor from 'App/Models/Professor'
 
-export default class ProfessoresController {}
+export default class ProfessoresController {
+     public async store({request, response}: HttpContextContract){
+
+        const body = request.body()
+        const professor = await Professor.create(body)
+        response.status(201)
+
+        return{
+            message: 'Professor criado com sucesso',
+            data: professor,
+        }
+    }
+    public async  index() {
+        const professores = await Professor.all()
+
+        return{ data:professores }
+        }
+    public async show({params}: HttpContextContract){
+        const professor = await Professor.findOrFail(params.id)
+        return{ 
+            data: professor
+        }
+    }
+
+    public async destroy({params}: HttpContextContract){
+        const professor = await Professor.findOrFail(params.id)
+        await professor.delete()
+        return{ 
+            message: "Professor excluido com sucesso",
+            data: professor
+        }
+
+    }
+
+    public async update({params, request}: HttpContextContract){
+        const body = request.body()
+        const professor = await Professor.findOrFail(params.id)
+        professor.nome = body.nome
+        professor.email = body.email
+        professor.matricula = body.matricula
+        professor.data_nascimento = body.data_nascimento
+        await professor.save()
+
+        return{
+            message: "Professor atualizado com sucesso!",
+            data : professor,
+        }
+
+
+    }
+
+
+    
+
+
+}
