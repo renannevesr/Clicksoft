924f03d8bcc7bf88ca3047f22bce7db5899d7f8d
Renan Rego <renannevesr@gmail.com>
2023-03-18 10:16:08 -0300

    Models e migrations de professores

---
+++ app/Models/Professor.ts
@@ -0,0 +1,20 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class Professor extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+  @column ()
+  public nome: string
+  @column ()
+  public email: string
+  @column ()
+  public matricula: string
+  @column ()
+  public nascimento: Date
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
