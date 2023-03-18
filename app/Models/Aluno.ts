3fdee5964cf7fef91bb19de6d346dd5f8acf0ed4
Renan Rego <renannevesr@gmail.com>
2023-03-18 09:55:32 -0300

    Models e migrations de alunos

---
+++ app/Models/Aluno.ts
@@ -0,0 +1,22 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class Aluno extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column ()
+  public nome: string
+  @column ()
+  public email: string
+  @column ()
+  public matricula: string
+  @column ()
+  public nascimento: Date
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
--- start/routes.ts
+++ start/routes.ts
@@ -19,7 +19,11 @@
 */
 
 import Route from '@ioc:Adonis/Core/Route'
+Route.group(()=> {
+  Route.get('/', async () => {
+    return { hello: 'world' }
+  })
+  
+  
+}).prefix('/api')
 
-Route.get('/', async () => {
-  return { hello: 'world' }
-})
