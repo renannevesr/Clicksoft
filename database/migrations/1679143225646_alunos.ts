e878c6f1810f9454373315efe475f3ea0034d72b
Renan Rego <renannevesr@gmail.com>
2023-03-18 11:13:15 -0300

    ajuste migration alunos

--- database/migrations/1679143225646_alunos.ts
+++ database/migrations/1679143225646_alunos.ts
@@ -6,17 +6,10 @@ export default class extends BaseSchema {
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.string('nome')
-      table.string('email')
-      table.string('matricula')
-      table.string('nascimento')
-
-
-
-
-      /**
-       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
-       */
+      table.string('nome').notNullable()
+      table.string('email').notNullable()
+      table.string('matricula').notNullable()
+      table.date('dataNascimento').notNullable()
       table.timestamp('created_at', { useTz: true })
       table.timestamp('updated_at', { useTz: true })
     })
