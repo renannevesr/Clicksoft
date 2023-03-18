3b74814103a28f600a0ae74b6cea7eb695cf44e2
Renan Rego <renannevesr@gmail.com>
2023-03-18 12:00:58 -0300

    Controller criação de aluno

--- app/Controllers/Http/AlunosController.ts
+++ app/Controllers/Http/AlunosController.ts
@@ -1,8 +1,17 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Aluno from 'App/Models/Aluno'
 
 export default class AlunosController {
-    public async store(){
-        return { msg: "deucerto",}
+    public async store({request, response}: HttpContextContract){
+
+        const body = request.body()
+        const aluno = await Aluno.create(body)
+        response.status(201)
+
+        return{
+            message: 'Aluno criado com sucesso',
+            data: aluno,
+        }
     }
 
 }
